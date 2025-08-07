80a0bf3205becc66a3e772409fd322113d53fab3
Tashi Wangyel <bajayfs@gmail.com>
2025-08-07 12:55:07 +0600

    changes

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require("express");
-
+const path = require("path")
 const app = express()
+
+const viewRoutes = require('./routes/viewRoutes')
+
+app.use(express.static('public'))
+app.use("/public/images", express.static(__dirname + "/public/images"));
+
+
+app.use('/', viewRoutes)
+
+
+app.use(express.static(path.join(__dirname, 'views')))
 
 module.exports = app
---
+++ controllers/viewController.js
@@ -0,0 +1,49 @@
+const path = require("path")
+
+exports.getLoginForm=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'Login.html'))
+}
+
+exports.getSignupForm=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'Signup.html'))
+}
+
+exports.getIndexPage=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'index.html'))
+}
+
+exports.getProductPage=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'product.html'))
+}
+
+exports.getCartPage=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'addtocart.html'))
+}
+
+exports.getAdminLoginForm=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'admin', 'LoginForm.html'))
+}
+
+exports.getAdminDashboard=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'admin', 'dashboard.html'))
+}
+
+exports.getAdminConfirmedOrders=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'admin', 'ConfirmedOrders.html'))
+}
+
+exports.getAdminAddProduct=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'admin', 'AddProduct.html'))
+}
+
+exports.getAdminNewOrders=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'admin', 'NewOrders.html'))
+}
+
+exports.getAdminProducts=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'admin', 'Products.html'))
+}
+
+exports.getAdminUpdateProduct=(req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'admin', 'UpdateProduct.html'))
+}

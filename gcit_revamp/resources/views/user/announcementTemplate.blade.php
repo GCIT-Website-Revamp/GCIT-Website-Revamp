ef9f448af151a9f081fc16a00b37f3c59213c0ef
Thogmey <developer.thogmey@gmail.com>
2025-11-30 15:45:23 +0600

    announcement fix

--- gcit_revamp/resources/views/user/announcementTemplate.blade.php
+++ gcit_revamp/resources/views/user/announcementTemplate.blade.php
@@ -22,7 +22,7 @@
     </div>
 </div>
 <div class="pageContentWrapper">
-    <div class="section">
+    <div class="section eventsWrapper">
         <div class="mainContent announcementContent">
             <div class="card">
                 <div class="cardContent">
